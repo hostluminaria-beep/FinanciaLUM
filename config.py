8551432007eeef18ac325806bf1720ff32d598dd
hostluminaria-beep <hostluminaria@gmail.com>
2026-07-05 21:38:48 -0400

    Update config.py

--- config.py
+++ config.py
@@ -3,5 +3,3 @@ import os
 TOKEN = os.environ.get("TELEGRAM_TOKEN", "")
 ADMIN_ID = int(os.environ.get("ADMIN_ID", "0"))
 MASTER_PASSWORD = os.environ.get("MASTER_PASSWORD", "")
-GITHUB_TOKEN = os.environ.get("GITHUB_TOKEN", "")
-GITHUB_REPO = os.environ.get("GITHUB_REPO", "")
